c1bafc9d8860852d12e4899f6a53c51524d63929
Thubelihle Dlamini <thubelihledlamini88@gmail.com>
2025-09-13 13:14:18 +0200

    Update Teacher.ts

--- 0x04-TypeScript/task_4/js/subjects/Teacher.ts
+++ 0x04-TypeScript/task_4/js/subjects/Teacher.ts
@@ -1,7 +1,6 @@
-export interface Teacher {
-  firstName: string;
-  lastName: string;
-  experienceTeachingC?: number;
-  experienceTeachingReact?: number;
-  experienceTeachingJava?: number;
+namespace Subjects {
+  export interface Teacher {
+    firstName: string;
+    lastName: string;
+  }
 }
