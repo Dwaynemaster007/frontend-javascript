38184cc45174fc4a5a8658be467d06ac2d72e2d1
Thubelihle Dlamini <thubelihledlamini88@gmail.com>
2025-09-13 13:19:41 +0200

    Update Java.ts

--- 0x04-TypeScript/task_4/js/subjects/Java.ts
+++ 0x04-TypeScript/task_4/js/subjects/Java.ts
@@ -1,6 +1,11 @@
-import { Subject } from './Subject';
-import { Teacher } from './Teacher';
+// Add the declaration merging for the Teacher interface
+namespace Subjects {
+  export interface Teacher {
+    experienceTeachingJava?: number;
+  }
+}
 
+// All other imports and class code should follow
 export class Java extends Subject {
   getRequirements(): string {
     return 'Here is the list of requirements for Java';
